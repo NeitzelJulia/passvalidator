e07b260f712cbbc519aff22dcd421bab05d17f5e
NeitzelJulia <julia.neitzel86@gmail.com>
2025-07-01 15:45:35 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    //ToDo checkPasswordLength
+    public static boolean checkPasswordLength(String password) {
+        return true;
+    }
+
+    //ToDo hasDigits
+    public static boolean hasDigits(String password) {
+        return true;
+    }
+
+    //ToDo hasUpperAndLowerCase
+    public static boolean hasUpperAndLowerCase(String password) {
+        return true;
+    }
+
+    //Todo isBlacklistedPassword
+    public static boolean hasSpecialCharacters(String password) {
+        return true;
+    }
+}
